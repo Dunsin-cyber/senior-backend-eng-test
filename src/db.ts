205197324c30b9b994b67748b2fc9533e7b3ba51
Oluwadunsin <ayodejiabisuwa23@gmail.com>
2023-08-24 08:47:41 +0100

    [feat] - added table queries

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,5 @@
 import { Client } from 'pg';
+import { v4 as uuidv4 } from 'uuid';
 import {
   POSTGRES_DB,
   POSTGRES_PASSWORD,
@@ -13,11 +14,48 @@ const db = new Client({
   port: 5432, // Default PostgreSQL port
 });
 
+async function createTables() {
+  try {
+    // Enable the uuid-ossp extension
+    await db.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+
+    // Creates Tables
+    const createTablesQuery = `
+        CREATE TABLE IF NOT EXISTS users (
+          user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+          username VARCHAR(255) NOT NULL,
+          email VARCHAR(255) NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS posts (
+          post_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+          user_id UUID REFERENCES users(user_id),
+          title VARCHAR(255) NOT NULL,
+          content TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS comments (
+          comment_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+          post_id UUID REFERENCES posts(post_id),
+          user_id UUID REFERENCES users(user_id),
+          content TEXT
+        );
+      `;
+    await db.query(createTablesQuery);
+    console.log('Tables created successfully');
+  } catch (error) {
+    console.error('Error creating tables:', error);
+  } finally {
+    db.end();
+  }
+}
+
 //Establish conection to the db.
 const checkDBConnection = () => {
   db.connect()
     .then(() => {
-      console.log('Connected to the database');
+      console.log('Connected to the database, creating tables...');
+      createTables();
     })
     .catch((error) => {
       console.error(
